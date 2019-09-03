8457d80cf248c19085b9fbb8eae07f518644d2e9
ECortesHe <ECortesHe@MXZ00L8258TC2.zurich.mx>
2019-09-03 13:54:56 -0500

    primer commit

---
+++ src/main/java/com/mx/app/base/data/dummy/DummyDataProvider.java
@@ -0,0 +1,44 @@
+package com.mx.app.base.data.dummy;
+
+import com.mx.app.base.data.DataProvider;
+import com.mx.app.base.domain.User;
+import java.util.Calendar;
+import java.util.Date;
+
+
+/**
+ * A dummy implementation for the backend API.
+ */
+public class DummyDataProvider implements DataProvider {
+    
+    private static Date lastDataUpdate;
+
+/**
+     * Initialize the data for this application.
+     */
+    public DummyDataProvider() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        if (lastDataUpdate == null || lastDataUpdate.before(cal.getTime())) {
+            //refreshStaticData();
+            lastDataUpdate = new Date();
+        }
+    }
+
+    @Override
+    public User authenticate(String userName, String password) {
+        User user = new User();
+        user.setFirstName(DummyDataGenerator.randomFirstName());
+        user.setLastName(DummyDataGenerator.randomLastName());
+        user.setRole("admin");
+        String email = user.getFirstName().toLowerCase() + "."
+                + user.getLastName().toLowerCase() + "@"
+                + DummyDataGenerator.randomCompanyName().toLowerCase() + ".com";
+        user.setEmail(email.replaceAll(" ", ""));
+        user.setLocation(DummyDataGenerator.randomWord(5, true));
+        user.setBio("Quis aute iure reprehenderit in voluptate velit esse."
+                + "Cras mattis iudicium purus sit amet fermentum.");
+        return user;
+    }
+
+}
---
+++ src/main/java/com/mx/app/base/event/AppEvent.java
@@ -0,0 +1,52 @@
+package com.mx.app.base.event;
+
+import com.mx.app.base.view.AppViewType;
+
+
+public abstract class AppEvent {
+
+    public static final class UserLoginRequestedEvent {
+        private final String userName, password;
+
+        public UserLoginRequestedEvent(final String userName,
+                final String password) {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    public static class BrowserResizeEvent {
+
+    }
+
+    public static class UserLoggedOutEvent {
+
+    }
+    
+    public static final class PostViewChangeEvent {
+        private final AppViewType view;
+
+        public PostViewChangeEvent(final AppViewType view) {
+            this.view = view;
+        }
+
+        public AppViewType getView() {
+            return view;
+        }
+    }
+   
+    public static class CloseOpenWindowsEvent {
+    }
+
+    public static class ProfileUpdatedEvent {
+    }
+
+}
